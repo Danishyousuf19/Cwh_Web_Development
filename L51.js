f85a28a154d894e1f34bcac94f8f1766eb71b33b
Danish Yousuf <130566129+Danishyousuf19@users.noreply.github.com>
2023-08-21 14:56:00 +0530

    Add files via upload

---
+++ L51.js
@@ -0,0 +1,28 @@
+var str1="this is a str"
+// let str1="this is a str let"
+console.log(str1)
+let a=4;
+{
+let a=8;
+console.log(a);
+}
+const x="no change"
+console.log(a);
+console.log(x );
+
+let age=33;
+if(age>18){
+console.log("hi")
+}
+else{
+console.log("bt")
+}
+switch (age) {
+case 23:
+console.log("hi")
+break;
+
+default:
+console.log("by")
+break;
+}
---
+++ L52.js
@@ -0,0 +1,22 @@
+let arr={
+    "danish":"name",
+    "class":"6th",
+    "age":4
+
+};
+
+let arry=[1,3,"danis",null,undefined,9];
+let ar=new Array(1,3,"danish yousuf");
+let a=new Array(13);
+arry.push("pushed" ,4);
+arry.sort;
+// ar.delete(3);
+console.log(arry);
+console.log(arr);
+console.log(ar);
+console.log(a);
+console.log(arry[1]);
+console.log(arry.length);
+
+
+
