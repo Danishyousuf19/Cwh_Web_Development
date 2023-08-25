135d917815a808f5bbc66a7c83594bdf163471c1
Danish Yousuf <130566129+Danishyousuf19@users.noreply.github.com>
2023-08-25 12:26:29 +0530

    Add files via upload

---
+++ L53.js
@@ -0,0 +1,12 @@
+console.log("hi");
+
+function greet(name,time){
+    console.log(name+" bye "+time )
+}
+let name1="danish";
+function ret(name="sum = "){
+    console.log(name+" bye "+time )
+    return 33;
+}
+greet(name1,23);
+ret();
